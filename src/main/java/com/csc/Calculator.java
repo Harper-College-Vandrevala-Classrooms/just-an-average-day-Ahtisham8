ef03d478c6f9e2ed71fd8acf4b230812ba77ffa2
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-12-08 21:00:41 -0600

    Finished lab
    Created eight total functions that calculate max, min, sum, and avg by both using for loops and streams.

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,6 +1,81 @@
 package com.csc;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Calculator {
+  
+  public int maximumUsingForLoop(int[] nums) 
+  {
+    int max = nums[0];
+    for(int i = 0; i < nums.length; i++)
+    {
+      if (max < nums[i]) {
+        max = nums[i];
+      }
+    }
+
+    return max;
+  }
+  public int minimumUsingForLoop(int[] nums) 
+  { 
+    int min = nums[0];
+    for(int i = 0; i < nums.length; i++)
+    {
+      if (min > nums[i]) {
+        min = nums[i];
+      }
+    }
+
+    return min;
+  }
+  public int sumUsingForLoop(int[] nums) 
+  {
+    int sum = 0;
+    for(int i = 0; i < nums.length; i++)
+    {
+      sum += nums[i];
+    }
+
+    return sum;
+  }
+  public int averageUsingForLoop(int[] nums) 
+  {
+    int quantity = nums.length;
+    int sum = 0;
+    int avg = 0;
+    for(int i = 0; i < nums.length; i++)
+    {
+      sum += nums[i];
+    }
+
+    avg = sum/quantity;
+
+    return avg;
+
+  }
+  
+  public int maximumUsingStream(int[] nums) 
+  {
+    int max = Arrays.stream(nums).max().getAsInt();
+    return max;
+  }
+  public int minimumUsingStream(int[] nums) 
+  {
+    int min = Arrays.stream(nums).min().getAsInt();
+    return min;
+  }
+  public int sumUsingStream(int[] nums) 
+  {
+    int sum = Arrays.stream(nums).reduce(0, (x,y) -> x+y);
+    return sum;
+  }
+  public double averageUsingStream(int[] nums) 
+  {
+    double avg = Arrays.stream(nums).mapToDouble(no->no).average().getAsDouble();
+    return avg;
+  }
+
   public static void main(String[] args) {
     System.out.println("This class can house your driver program and / or your functions.");
   }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -18,4 +18,47 @@ public class TestCalculator {
   void itWorks() {
     assertEquals(true, true);
   }
+
+  @Test
+  void testMaximumFunctionForLoop() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(50, calculator.maximumUsingForLoop(numbers));
+  }
+  @Test
+  void testMinimumFunctionForLoop() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(10, calculator.minimumUsingForLoop(numbers));
+  }
+  @Test
+  void testSumFunctionForLoop() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(150, calculator.sumUsingForLoop(numbers));
+  }
+  @Test
+  void testAvgFunctionForLoop() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(30, calculator.averageUsingForLoop(numbers));
+  }
+
+
+  @Test
+  void testMaximumStreams() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(50, calculator.maximumUsingStream(numbers));
+  }
+  @Test
+  void testMinimumStreams() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(10, calculator.minimumUsingStream(numbers));
+  }
+  @Test
+  void testsumStreams() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(150, calculator.sumUsingStream(numbers));
+  }
+  @Test
+  void testAvgStreams() {
+    int[] numbers = {10, 20, 30, 40, 50};
+    assertEquals(30, calculator.averageUsingStream(numbers));
+  }
 }
